aaaa58a16e3d8f24f75eb2b2876751edf2b14c84
Nishant Kyal <nishant.kyal@gmail.com>
2026-07-08 22:32:20 +0530

    removed clarity sdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,13 +121,13 @@ export default function RootLayout({
         </noscript>
 
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="lazyOnload">
+        {/* <Script id="clarity" strategy="lazyOnload">
           {`(function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "v9f9xqikhz");`}
-        </Script>
+        </Script> */}
 
         <Script id="posthog" strategy="afterInteractive">
           {`window.posthog = window.posthog && typeof window.posthog.push === "function" ? window.posthog : [];
